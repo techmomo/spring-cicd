4083fc1708c3ede0784af4c82be4dcf7041380af
techmomo <mohsin.khursheed@techjini.com>
2021-07-30 12:11:07 +0530

    Rename main file

--- src/main/java/com/mohsinkd786/Application.java
+++ src/main/java/com/mohsinkd786/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCicdApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCicdApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
